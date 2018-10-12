2010b79a3297419a3fe06a38b650e6b473bab421
Christian Müller <cmueller.dev@gmail.com>
2018-10-12 20:27:50 +0200

    Add AGPLv3 License

--- ui/src/app/calculation/calculation.component.ts
+++ ui/src/app/calculation/calculation.component.ts
@@ -1,3 +1,19 @@
+// ogwc (https://github.com/c-mueller/ogwc).
+// Copyright (c) 2018 Christian Müller <cmueller.dev@gmail.com>.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute, Router} from '@angular/router';
 import {ApiService} from '../svc/api.service';
@@ -14,6 +30,8 @@ export class CalculationComponent implements OnInit {
   public calculation: CalculationResponse = null;
   public participants: Participant[] = [];
 
+  public apiKey: string;
+
   constructor(private route: ActivatedRoute,
               private router: Router,
               private api: ApiService) {
@@ -22,14 +40,29 @@ export class CalculationComponent implements OnInit {
   ngOnInit() {
     this.route.paramMap.subscribe(p => {
       this.calculationID = p.get('id');
-      this.api.fetchCalculation(this.calculationID).subscribe(e => {
-        this.calculation = e;
-        this.participants = this.calculation.participants;
-      }, err => {
-        console.log(err);
-        this.router.navigate(['/404']);
-      });
+      this.getCalculation();
     });
+  }
+
+  private getCalculation() {
+    this.api.fetchCalculation(this.calculationID).subscribe(e => {
+      this.calculation = e;
+      this.participants = this.calculation.participants;
+    }, err => {
+      console.log(err);
+      this.router.navigate(['/404']);
+    });
+  }
+
+  addAPIKey() {
+    this.api.addApiKey(this.apiKey, this.calculationID).subscribe(e => {
+      this.getCalculation();
+      alert('API Key wurde Hinzugefügt');
+    }, err => {
+      console.log(err);
+      alert('API Key wurde nicht Hinzugefügt. Es ist ein Fehler aufgetreten.');
+    });
+    this.apiKey = '';
   }
 
   getShortLink() {
